2a108e9d2c137cd9a04cde3c489b718c0e5d9b25
Chris Lacey <clacey@fusionalliance.com>
2023-05-31 16:34:15 -0400

    quick test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import FormDisplay from '@/components/FormDisplay'
 import { AssessmentData } from './api/assessment/route'
-const VERCEL_URL = '/blueprint-swart.vercel.app'
-function getBaseUrl () {
-  if (typeof window !== 'undefined') return '' // browser should use relative url
-  // if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-  // if not in dev move use vercel url
-  if (process.env.NODE_ENV !== 'development') return `https://${VERCEL_URL}` // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
-}
+
 export default async function Assessment () {
-  const assessmentReq = await fetch(`${getBaseUrl()}/api/assessment`)
+  const assessmentReq = await fetch('http://localhost:3000/api/assessment')
   const assessmentData = (await assessmentReq.json()) as AssessmentData
 
   return (
