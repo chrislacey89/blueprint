fff83618ab19bbf747b81af16cc03dbb51b384b0
Chris Lacey <clacey@fusionalliance.com>
2023-05-31 16:50:20 -0400

    refactor fetching from api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import FormDisplay from '@/components/FormDisplay'
-import { AssessmentData } from './api/assessment/route'
-
+import { AssessmentData, GET as getAssessment } from './api/assessment/route'
 export default async function Assessment () {
-  const assessmentReq = await fetch('http://localhost:3000/api/assessment')
+  const assessmentReq = await getAssessment()
   const assessmentData = (await assessmentReq.json()) as AssessmentData
 
   return (
